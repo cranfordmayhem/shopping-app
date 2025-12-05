8bafded559bd0584579177db81a7ef2a108ea729
Avel Kristian Ladano <avelkristian321@gmail.com>
2025-12-05 10:32:29 +0800

    fix: remove ALTER TABLE for `user_account` and `user_profile` in v3, already applied directly on the tables

--- src/main/resources/db/migration/V3__alter_table_for_audit_logging.sql
+++ src/main/resources/db/migration/V3__alter_table_for_audit_logging.sql
@@ -1,15 +1,3 @@
-ALTER TABLE user_account
-ADD COLUMN created_by VARCHAR(255),
-ADD COLUMN created_date TIMESTAMP,
-ADD COLUMN last_modified_by VARCHAR(255),
-ADD COLUMN last_modified_date TIMESTAMP;
-
-ALTER TABLE user_profile
-ADD COLUMN created_by VARCHAR(255),
-ADD COLUMN created_date TIMESTAMP,
-ADD COLUMN last_modified_by VARCHAR(255),
-ADD COLUMN last_modified_date TIMESTAMP;
-
 ALTER TABLE refresh_token
 ADD COLUMN created_by VARCHAR(255),
 ADD COLUMN created_date TIMESTAMP,
