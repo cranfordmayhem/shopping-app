0e3d34a4709c3939595f735fa201049539ed6c05
Avel Kristian Ladano <avelkristian321@gmail.com>
2025-12-05 08:53:29 +0800

    feat: commit profile entity and repository

---
+++ src/main/kotlin/com/example/shopping_app/repository/UserProfileRepository.kt
@@ -0,0 +1,8 @@
+package com.example.shopping_app.repository
+
+import com.example.shopping_app.entity.UserProfile
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserProfileRepository: JpaRepository<UserProfile, Long>
