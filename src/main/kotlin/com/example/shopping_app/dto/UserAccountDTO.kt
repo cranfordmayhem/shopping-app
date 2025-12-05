6f3c2f3aa1e9bc9cf7e60bb3ca0112ea3f9bdab5
Avel Kristian Ladano <avelkristian321@gmail.com>
2025-12-05 09:28:09 +0800

    feat: introduce validation constraints for UserAccountDTO and UserProfileRequestDTO

--- src/main/kotlin/com/example/shopping_app/dto/UserAccountDTO.kt
+++ src/main/kotlin/com/example/shopping_app/dto/UserAccountDTO.kt
@@ -1,9 +1,14 @@
 package com.example.shopping_app.dto
 
 import com.example.shopping_app.entity.enums.Role
+import jakarta.validation.constraints.*
 
 data class UserAccountRequest(
+    @field:NotBlank(message="Email is required")
+    @field:Email(message="Email must be valid")
     val email: String,
+
+    @field:NotBlank(message="Password is required")
     val password: String
 )
 
--- src/main/kotlin/com/example/shopping_app/dto/UserProfileDTO.kt
+++ src/main/kotlin/com/example/shopping_app/dto/UserProfileDTO.kt
@@ -1,8 +1,15 @@
 package com.example.shopping_app.dto
 
+import jakarta.validation.constraints.*
+
 data class UserProfileRequest(
+    @field:NotBlank(message="First name is required")
     val firstName: String,
+
+    @field:NotBlank(message="Last name is required")
     val lastName: String,
+
+    @field:Min(value=1, message="Age must be at least 1")
     val age: Int
 )
 
